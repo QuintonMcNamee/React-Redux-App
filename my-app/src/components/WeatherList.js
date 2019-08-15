637aa5f29d72e36a559bca44c3549898150aa607
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-08-15 16:49:34 -0400

    built component files

---
+++ my-app/src/components/WeatherList.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Loader from 'react-loader-spinner';
+
+import { getData } from '../actions';
+
+import Weather from './Weather';
+
+const WeatherList = props => {
+  return (
+    <>
+      <h1>Welcome to the weather</h1>
+      <button onClick={props.getData}>
+        {props.isLoading ? (
+          <Loader
+           type="Plane"
+           color="#00BFFF"
+           height="100"
+           width="100"
+          /> ) : (
+            'Get Weather Data'
+          )}
+      </button>
+      {props.weathers &&
+        props.weathers.map(item => <Weather key={item.name} weather={item} /> )}
+    </>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    isLoading: state.isLoading,
+    weathers: state.weathers
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { getData }
+)(WeatherList);
